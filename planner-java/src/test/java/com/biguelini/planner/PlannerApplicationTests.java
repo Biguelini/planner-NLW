fb9cbc623c81aeedc6b057d7460f75ce0ba6d11d
Biguelini <biguelinijoao@gmail.com>
2024-07-08 22:48:40 -0300

    aula 01 java

---
+++ planner-java/src/test/java/com/biguelini/planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biguelini.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
